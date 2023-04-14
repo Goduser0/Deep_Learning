1387e3931379bf4347b42581b47413117c22cf87
zhouquan <zhouquan@hust.edu.cn>
2023-04-14 20:56:12 +0800

    0414

---
+++ My_TAOD/my_dataloader.py
@@ -0,0 +1,83 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader, random_split
+from PIL import Image
+import numpy as np
+import os
+import pandas as pd
+import torchvision.transforms as transforms
+from d2l import torch as d2l
+import matplotlib.pyplot as plt
+
+def bmp_to_ndarray(file_path):
+    """将BMP文件转为ndarray"""
+    img = Image.open(file_path).convert('L')
+    img_array = np.array(img)
+    return img_array
+
+
+class My_Dateset(Dataset):
+    def __init__(self, root_dir, transform=None, target_transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.samples = self.load_samples()
+    
+    def __len__(self):
+        return len(self.samples)
+    
+    def __getitem__(self, idx):
+        img = self.samples.loc[idx, 'Image_Content']
+        label = self.samples.loc[idx, 'Image_Class']
+        labels = ['Cr', 'PS', 'In', 'RS', 'Sc', 'Pa']
+        for i in range(6):
+            if label == labels[i]:
+                label = i
+        label = np.array(label)
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return img, label
+
+    def load_samples(self):
+        sample_list = []
+        for filename in os.listdir(self.root_dir):
+            filename_list = filename.split('.')
+            if filename_list[1] == 'bmp':
+                # image_filename
+                img_filename = filename
+                # image_path
+                img_path = os.path.join(self.root_dir, filename)
+                # image(ndarray)
+                img = bmp_to_ndarray(img_path)
+                # image_class
+                img_class = filename_list[0][:2]
+                img_classID = filename_list[0][3:]
+                image_item = [img, img_class, img_filename, img_path, img_classID]
+                sample_list.append(image_item)
+            else:
+                continue
+        return pd.DataFrame(sample_list, columns=['Image_Content', 'Image_Class', 'Image_Filename', 'Image_Path', 'Image_ClassID'])
+
+trans = torchvision.transforms.ToTensor()
+# iamge_size[1, 200, 200]
+My_NEUCLS = My_Dateset('./My_Datasets/NEU-CLS', transform=trans)
+# My_NEUCLS.samples.to_csv('My_NEUCLS.csv')
+
+from ResNet import net
+
+batch_size = 128
+lr = 0.05
+num_epochs = 100
+
+train_size = int(0.7*len(My_NEUCLS))
+test_size = len(My_NEUCLS) - train_size
+nue_train, nue_test = random_split(My_NEUCLS, [train_size, test_size])
+
+train_iter = DataLoader(nue_train, batch_size, shuffle=True)
+test_iter = DataLoader(nue_test, batch_size, shuffle=False)
+
+from train import train
+
+train(net, train_iter, test_iter, num_epochs, lr, torch.device('cuda:1'))
