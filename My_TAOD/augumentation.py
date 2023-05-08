98e5a0b3b9e8f03b8a4fc4ce160492c278850bf2
noelstallworth_3080 <3080@noel.com>
2023-05-08 23:02:23 +0800

    add cyclegan _concise

---
+++ My_TAOD/augumentation.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+class Generator(nn.Module):
+    def __init__(self, inpput_size):
+        
+        
+    
