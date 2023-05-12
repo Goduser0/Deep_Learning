c00bb18dce6341d1e83687c417ddd81addf9e1e1
noelstallworth_3080 <3080@noel.com>
2023-05-12 11:26:59 +0800

    Create dzw_fsgan_final

--- My_TAOD/augumentation.py
+++ My_TAOD/augumentation.py
@@ -5,7 +5,19 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 class Generator(nn.Module):
-    def __init__(self, inpput_size):
-        
-        
+    def __init__(self):
+        pass
     
+    def forward(self,):
+        pass
+    
+class Discriminator(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self):
+        pass
+    
+if __name__ == "__main__":
+    G = Generator()
+    D = Discriminator()
--- test.py
+++ test.py
@@ -1,3 +1,32 @@
-L = [1, 2, 3]
-sublist = [L[i:i+j] for i in range(0,len(L)) for j in range(1,len(L)-i+1)]
-print(sublist)
+import time
+import numpy as np
+class Timer(object):
+    """Create a Timer, Record the time gap"""
+    def __init__(self):
+        self.times=[]
+        self.start()
+        
+    def start(self):
+        # record the timestamp of start
+        self.tik = time.time()
+        
+    def stop(self):
+        # record the time gap from start to stop
+        self.times.append(time.time() - self.tik)
+        
+    def avg(self):
+        # 求平均值
+        return sum(self.times) / len(self.times)
+    
+    def sum(self):
+        # 求和
+        return sum(self.times)
+    
+    def cumsum(self):
+        # 求累积和
+        return np.array(self.times).cumsum().tolist()
+    
+timer = Timer()
+timer.stop()
+timer.stop()
+print(timer.times[0], timer.times[1])
