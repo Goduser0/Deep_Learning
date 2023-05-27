c9247052258ea42b4a3fd130611ad9541dcd0ada
3080 <3080@noel.com>
2023-05-27 15:24:12 +0800

    1

--- My_TAOD/augumentation.py
+++ My_TAOD/augumentation.py
@@ -1,8 +1,34 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.utils.data as data
+import torchvision.transforms as T
+from torch import optim
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+##########################################################################################################
+# FUNCTION:show_images
+##########################################################################################################
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """Plot a list of images."""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            # Tensor Image
+            ax.imshow(img.numpy())
+        else:
+            # PIL Image
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
 
 class Generator(nn.Module):
     def __init__(self):
@@ -21,3 +47,8 @@ class Discriminator(nn.Module):
 if __name__ == "__main__":
     G = Generator()
     D = Discriminator()
+    
+a = Image.open('./My_Datasets/Classification/NEU-CLS/Pa_299.bmp')
+b = Image.open('./My_Datasets/Classification/NEU-CLS/Pa_299.bmp')
+axes = show_images([a, b], 1, 2)
+plt.imshow(a)
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data as data
+import torchvision.transforms as T
+from torch import optim
 
-print("Hello World!!!")
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+a = Image.open('./My_Datasets/Classification/NEU-CLS/Pa_299.bmp')
+
+plt.imshow(a)
