44aa3bae9db8e2b5b69467c9c656f361d7a3de19
3080 <3080@noel.com>
2023-05-31 17:18:17 +0800

    3080

--- My_TAOD/augumentation.py
+++ My_TAOD/augumentation.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as data
@@ -9,3 +10,4 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
+
