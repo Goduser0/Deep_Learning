a55215193701274ce43d6784b67273ca3714967b
3090 <3090@noel.com>
2023-06-05 18:12:40 +0800

    demo

--- demo.py
+++ demo.py
@@ -5,19 +5,12 @@ import torch.utils.data as data
 import torchvision.transforms as T
 from torch import optim
 
+import os
 import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
 
-a = [{"a":1, "b":1}, {"a":2, "b":2}]
-a = pd.DataFrame(a)
-a.to_csv('a.csv', index=False)
-a = pd.read_csv('a.csv')
-
-for i in range(1):
-    df = pd.read_csv('a.csv')
-    df.to_csv('a.csv')
-
-a = pd.read_csv('a.csv')
-print(a)
+file_list = os.listdir('./My_Datasets/Classification/Magnetic-Tile-Defect')
+file_list = [filename for filename in file_list if filename[:2]=='MT']
+print(file_list)
