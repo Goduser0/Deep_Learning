b95d2eadaaabb20c115b414e62439474d4708759
Noel_3090 <3090@noel.com>
2025-03-06 22:31:10 +0800

    add plot

--- My_TAOD/Train_Classification/run_shell/cls_P2.sh
+++ My_TAOD/Train_Classification/run_shell/cls_P2.sh
@@ -1,4 +1,4 @@
-#* single-domain
+#! single-domain
 
 ENV="/home/zhouquan/anaconda3/envs/DLstudy/bin/python My_TAOD/Train_Classification/main.py"
 
@@ -41,32 +41,36 @@ ENV="/home/zhouquan/anaconda3/envs/DLstudy/bin/python My_TAOD/Train_Classificati
 # /home/zhouquan/anaconda3/envs/DLstudy/bin/python My_TAOD/Train_Classification/main.py --mode test --dataset_class PCB_200 --gpu_id "1" --dataset_ratio "160-shot" --classification_net "VGG11_Pretrained" --test_model_path "/home/zhouquan/MyDoc/Deep_Learning/My_TAOD/Train_Classification/results/train/cls_single_domain/PCB_200 VGG11_Pretrained 20241225_155942/models/100.pth"
 # /home/zhouquan/anaconda3/envs/DLstudy/bin/python My_TAOD/Train_Classification/main.py --mode test --dataset_class PCB_200 --gpu_id "1" --dataset_ratio "160-shot" --classification_net "MobileNet_Pretrained" --test_model_path "/home/zhouquan/MyDoc/Deep_Learning/My_TAOD/Train_Classification/results/train/cls_single_domain/PCB_200 MobileNet_Pretrained 20241225_160513/models/100.pth"
 
-# train 50-shot 70-shot 90-shot 110-shot 130-shot 150-shot
-MODELS=("Resnet18_Pretrained" "Resnet50_Pretrained" "EfficientNet_Pretrained" "VGG11_Pretrained" "MobileNet_Pretrained")
+#! train 50-shot 70-shot 90-shot 110-shot 130-shot 150-shot on all models
+# MODELS=("Resnet18_Pretrained" "Resnet50_Pretrained" "EfficientNet_Pretrained" "VGG11_Pretrained" "MobileNet_Pretrained")
 
-for MODEL in "${MODELS[@]}"; do
-    $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "50-shot" --classification_net "$MODEL"
+# for MODEL in "${MODELS[@]}"; do
+#     $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "50-shot" --classification_net "$MODEL"
+# done
+
+# for MODEL in "${MODELS[@]}"; do
+#     $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "70-shot" --classification_net "$MODEL"
+# done
+
+# for MODEL in "${MODELS[@]}"; do
+#     $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "90-shot" --classification_net "$MODEL"
+# done
+
+# for MODEL in "${MODELS[@]}"; do
+#     $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "110-shot" --classification_net "$MODEL"
+# done
+
+# for MODEL in "${MODELS[@]}"; do
+#     $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "130-shot" --classification_net "$MODEL"
+# done
+
+# for MODEL in "${MODELS[@]}"; do
+#     $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "150-shot" --classification_net "$MODEL"
+# done
+
+#! test 50-shot 70-shot 90-shot 110-shot 130-shot 150-shot on all models
+root_path="/home/zhouquan/MyDoc/Deep_Learning/My_TAOD/Train_Classification/results/PCB_200"
+mapfile -t subfolders < <(find "$root_path" -mindepth 1 -maxdepth 1 -type d)
+for folder in "${subfolders[@]}"; do
+  $ENV --mode "test" --gpu_id "1" --test_model_path "$folder/models/100.pth"
 done
-
-for MODEL in "${MODELS[@]}"; do
-    $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "70-shot" --classification_net "$MODEL"
-done
-
-for MODEL in "${MODELS[@]}"; do
-    $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "90-shot" --classification_net "$MODEL"
-done
-
-for MODEL in "${MODELS[@]}"; do
-    $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "110-shot" --classification_net "$MODEL"
-done
-
-for MODEL in "${MODELS[@]}"; do
-    $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "130-shot" --classification_net "$MODEL"
-done
-
-for MODEL in "${MODELS[@]}"; do
-    $ENV --mode "train" --dataset_class "PCB_200" --gpu_id "1" --dataset_ratio "150-shot" --classification_net "$MODEL"
-done
-
-#* test 50-shot
-# $ENV --mode "test" --gpu_id "1" --test_model_path "My_TAOD/Train_Classification/results/PCB_200 EfficientNet_Pretrained 50-shot 20250304_150611/models/100.pth"
